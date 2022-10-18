4fb282e87d8063fe13977c5052d1f3ac42d0158f
Christian Predoianu <christianpredoianu@yahoo.com>
2022-10-18 11:06:10 +0200

    added faq data

---
+++ js/faqData.js
@@ -0,0 +1,12 @@
+export const faqData = {
+  covid19:
+    'Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus. Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age.',
+  spreads:
+    'The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols. It is important to practice respiratory etiquette, for example by coughing into a flexed elbow, and to stay home and self-isolate until you recover if you feel unwell.',
+  protect:
+    'The best way to prevent and slow down transmission is to be well informed about the disease and how the virus spreads. Protect yourself and others from infection by staying at least 1 metre apart from others, wearing a properly fitted mask, and washing your hands or using an alcohol-based rub frequently. Get vaccinated when it’s your turn and follow local guidance.',
+  symptoms:
+    'The most common symptoms of Covid-19 are: fever, cough, tiredness, loss of taste or smell.',
+  community:
+    'The coronavirus disease 2019 (COVID-19) outbreak was declared a public health emergency of international concern on 30 January 2020. The situation continues to evolve rapidly in China and internationally, and further spread of the virus should be anticipated. This document is a guide for national authorities who are responsible for managing public health responses to COVID-19.',
+};
